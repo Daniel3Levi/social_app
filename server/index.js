2126b86d634ee2dcc3a89c69416c988b61b4396f
Daniel <daniel3levi@gmail.com>
2023-01-02 15:26:14 +0200

    Data Modeling, Authentication and Authorization.

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { register } from './controllers/auth.js';
+import authRouter from './routes/auth.js';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +37,12 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/*ROUTES WITH FILES (multer upload can be only in index.js)*/
+app.post('/auth/register', upload.single('picture'), register);
+
+/*ROUTES*/
+app.use('/auth', authRouter);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
